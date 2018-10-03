9461a64484162ff3175bb47d94bad05347c1f435
Nathan <nathanarend@hotmail.com>
2018-10-03 14:24:17 -0300

    Telas do projeto pré-prontas

--- src/main/java/br/com/unialfa/nupes/Main.java
+++ src/main/java/br/com/unialfa/nupes/Main.java
@@ -2,10 +2,9 @@ package br.com.unialfa.nupes;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -14,7 +13,6 @@ public class Main extends Application {
 		try {
 			AnchorPane root = FXMLLoader.load(getClass().getResource("./view/FXMLLogin.fxml"));
 			Scene scene = new Scene(root,774,521);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Login");
 			primaryStage.setScene(scene);
 			primaryStage.show();
