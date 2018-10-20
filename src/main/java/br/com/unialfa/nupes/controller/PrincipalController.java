2392b3153a35fd33f6e50aac8d947530219159c8
Thiago Sena <thiagom.sena97@gmail.com>
2018-10-20 14:50:22 -0300

    Adição classe ControllerPrincipal

--- src/main/java/br/com/unialfa/nupes/controller/PrincipalController.java
+++ src/main/java/br/com/unialfa/nupes/controller/PrincipalController.java
@@ -1,12 +1,17 @@
 package br.com.unialfa.nupes.controller;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 
 import com.jfoenix.controls.JFXButton;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class PrincipalController {
 	@FXML
@@ -17,11 +22,18 @@ public class PrincipalController {
 	private JFXButton btnRelatorio;
 	@FXML
 	private JFXButton btnDeslogar;
-	
-	
-	void cadastrar (ActionEvent event) {
+	@FXML
+	private AnchorPane panePrincipal;
+
+	void cadastrar(ActionEvent event) throws IOException {
 		URL arquivoFXML;
-		arquivoFXML = getClass().getResource();
+		arquivoFXML = getClass().getResource("/view/FXMLCadastro.fxml");
+		Parent fxmlParent = (Parent) FXMLLoader.load(arquivoFXML);
+		panePrincipal.getChildren().clear();
+		panePrincipal.getChildren().add(fxmlParent);
+		Scene scene = new Scene(fxmlParent, 774,521);
+	
+
 	}
 
 }
