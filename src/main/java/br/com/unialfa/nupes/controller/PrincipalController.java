768e9ece5762f0ec1351f62ee017463f7e1d0a72
Thiago Sena <thiagom.sena97@gmail.com>
2018-10-20 18:28:32 -0300

    Transição Tela Principal

--- src/main/java/br/com/unialfa/nupes/controller/PrincipalController.java
+++ src/main/java/br/com/unialfa/nupes/controller/PrincipalController.java
@@ -1,37 +1,42 @@
 package br.com.unialfa.nupes.controller;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 
 import com.jfoenix.controls.JFXButton;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 public class PrincipalController {
-	@FXML
-	private JFXButton btnCadastro;
-	@FXML
-	private JFXButton btnDocumentos;
-	@FXML
-	private JFXButton btnRelatorio;
-	@FXML
-	private JFXButton btnDeslogar;
-	@FXML
-	private AnchorPane panePrincipal;
+	   @FXML
+	    private AnchorPane PanePrincipal;
+
+	    @FXML
+	    private JFXButton btnDeslogar;
+
+	    @FXML
+	    private Button btnCadastro;
+
+	    @FXML
+	    private Button btnDocumentos;
+
+	    @FXML
+	    private Button btnRelatorio;
 
 	@FXML
 	void cadastrar(ActionEvent event) throws IOException {
 		URL arquivoFXML;
 		arquivoFXML = getClass().getResource("../view/FXMLCadastro.fxml");
 		Parent fxmlParent = (Parent) FXMLLoader.load(arquivoFXML);
-		panePrincipal.getChildren().clear();
-		panePrincipal.getChildren().add(fxmlParent);
-		Scene scene = new Scene(fxmlParent, 774, 521);
+		PanePrincipal.getChildren().clear();
+		PanePrincipal.getChildren().add(fxmlParent);
+		//Scene scene = new Scene(fxmlParent, 774, 521);
 
 	}
 
