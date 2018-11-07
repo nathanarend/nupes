4c275c1a53331dd83341fea7de860da7247bd806
Thiago Sena <thiagom.sena97@gmail.com>
2018-11-06 23:43:31 -0200

    Re-upando projeto para o git

--- bin/SQL/NUPES/BD.NUPES.sql
+++
@@ -1,8 +0,0 @@
-create database nupes;
-use nupes;
-create table usuario(
-id int(10) auto_increment primary key,
-usuario varchar(7) not null, 
-senha varchar(7) not null);
-describe  usuario;
-INSERT INTO usuario(usuario,senha) VALUES ('root','123');
--- src/main/java/br/com/unialfa/nupes/dao/DAOInterface.java
+++ src/main/java/br/com/unialfa/nupes/dao/DAOInterface.java
@@ -1,9 +1,11 @@
 package br.com.unialfa.nupes.dao;
 
+import java.sql.SQLException;
+
 import br.com.unialfa.nupes.entity.Aluno;
 import br.com.unialfa.nupes.entity.Curso;
 
 public interface DAOInterface<T,K> {
-	public void salvar(T obj,K ob);
+	public void salvar(T obj,K ob) throws SQLException;
 
 }
