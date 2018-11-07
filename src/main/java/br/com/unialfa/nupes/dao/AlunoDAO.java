de2cc5ed37bc0084126de18f48b9fdc77113bd00
Thiago Sena <thiagom.sena97@gmail.com>
2018-11-06 23:50:58 -0200

    23:50

--- src/main/java/br/com/unialfa/nupes/dao/AlunoDAO.java
+++ src/main/java/br/com/unialfa/nupes/dao/AlunoDAO.java
@@ -2,7 +2,9 @@ package br.com.unialfa.nupes.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import br.com.unialfa.nupes.configuration.ModuloConexao;
 import br.com.unialfa.nupes.entity.Aluno;
@@ -14,9 +16,9 @@ public class AlunoDAO implements DAOInterface<Aluno,Curso> {
 	Curso curs = new Curso();
 
 	private final String salvar1 = "INSERT INTO pessoa(nome,matricula) VALUES (?,?)";
-	private final String salvar2 = "INSERT INTO curso(Nome) VALUES (?)";
+	private final String salvar2 = "INSERT INTO curso(id,Nome) VALUES (?,?)";
 
-	public void salvar(Aluno aluno, Curso curso) {
+	public void salvar(Aluno aluno, Curso curso) throws SQLException {
 		// Aluno aluno = new Aluno();
 
 		PreparedStatement stmt = null;
@@ -24,15 +26,22 @@ public class AlunoDAO implements DAOInterface<Aluno,Curso> {
 
 		try {
 			conn.setAutoCommit(false);
-			stmt = conn.prepareStatement(salvar1);
-			
+
+			stmt = conn.prepareStatement(salvar1, Statement.RETURN_GENERATED_KEYS);
+
 			stmt.setString(1, aluno.getNome());
 			stmt.setString(2, aluno.getMatricula());
 			int rows = stmt.executeUpdate();
-			
+			ResultSet rs = stmt.getGeneratedKeys();
+			rs.next();
+
+			int IdGenerated = rs.getInt(1);
+			aluno.setId_curso(IdGenerated);
+
 			stmt = conn.prepareStatement(salvar2);
 			String c = curso.getCurso().getNomeCurso();
-			stmt.setString(1, c);
+			stmt.setInt(1, aluno.getId_curso());
+			stmt.setString(2, c);
 			stmt.executeUpdate();
 			conn.commit();
 			System.out.println("Salvo com sucesso!");
